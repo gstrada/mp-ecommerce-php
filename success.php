df43c48b2e2364a24b4a09cce7119756acbb4049
Guillermo Strada <guillermostrada@gmail.com>
2020-08-21 02:53:03 -0300

    Creo back_url_success

--- success.php
+++ success.php
@@ -14,8 +14,6 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
-    <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
-
     <script src="https://www.mercadopago.com/v2/security.js" view=""></script>
 
 
@@ -43,8 +41,7 @@
     <br>
     <div class="row" style="display: flex; justify-content: center">
         <div class="col-md-12 d-flex justify-content-center">
-            <a href="/index.php">
-                <button type="button" class="btn btn-primary">Volver al inicio</button></a>
+            <button type="button" class="btn btn-">Volver al inicio</button>
         </div>
     </div>
     <div class="as-footnotes" style="position: absolute; bottom: 0px; width: 100%">
