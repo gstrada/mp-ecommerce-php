be0afb465e83f798ca9376dc568704a32368b5f0
Guillermo Strada <guillermostrada@gmail.com>
2020-08-21 17:21:17 -0300

    Elimino archivo innecesario

--- notifications.php
+++
@@ -1,5 +0,0 @@
-<?php
-header('Content-Type: application/json');
-$request = $_REQUEST;//file_get_contents('php://input');
-$req_dump = print_r( $request, true );
-$fp = file_put_contents( 'request.log', $req_dump );
