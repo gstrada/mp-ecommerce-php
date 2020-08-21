a95b6b50956ad8309954b925d7512793d7102167
Guillermo Strada <guillermostrada@gmail.com>
2020-08-21 02:31:23 -0300

    Creo back_url_success

--- success.php
+++ success.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html class="supports-animation supports-columns svg no-touch no-ie no-oldie no-ios supports-backdrop-filter as-mouseuser"
+<html
       lang="en-US">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
