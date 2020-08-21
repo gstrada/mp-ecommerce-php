a8a61f5a936b97ae26536a1d67d81df07a8c34e7
Guillermo Strada <guillermostrada@gmail.com>
2020-08-21 02:16:59 -0300

    Creo back_url_success

--- success.php
+++ success.php
@@ -460,6 +460,8 @@
 
                         </div>
                     </div>
+                    </div>
+
                     <div class="container">
                         <div class="row">
                             <div class="col-md-12 d-flex justify-content-center">
@@ -468,7 +470,6 @@
                             </div>
                         </div>
 
-                    </div>
                     </div>
                 </div>
             </div>
