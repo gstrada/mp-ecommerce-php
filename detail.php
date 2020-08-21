4cfeff2370cba038cf0d9abddd2cb83314e16d5d
Guillermo Strada <guillermostrada@gmail.com>
2020-08-21 03:36:02 -0300

    Reformat details

--- detail.php
+++ detail.php
@@ -472,7 +472,7 @@
                                 <div class="images mini-gallery gal5 ">
                                     <?php
                                     // SDK de Mercado Pago
-                                    require '/vendor/autoload.php';
+                                    require __DIR__ .'/vendor/autoload.php';
 
                                     // Agrega credenciales
                                     MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
